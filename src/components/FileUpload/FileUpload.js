92262b159912402b89f42901163b99d484348967
Muthu Sree Prasanth <muthusreeprasanth@gmail.com>
2023-04-03 20:30:55 +0530

    added styles and handlers for File Upload

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,10 +1,21 @@
 import "./style.css";
 
 const FileUpload = () => {
+  const fileUploadHandler = (event) => {
+    console.log(event.target.files[0]);
+    // API call to send it to back end comes here
+  };
+
   return (
     <div className="fileUploadBox">
       <form>
-        <p>File upload comes here</p>
+        <p className="uploadLabel">File upload comes here</p>
+        <input
+          type="file"
+          onChange={fileUploadHandler}
+          className="uploadbox"
+        ></input>
+        <button className="uploadButton">Upload</button>
       </form>
     </div>
   );
